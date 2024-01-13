aca7b6bcb022552b3d15caf45a517b11156f943c
fx13013 <r_carlos013@hotmail.com>
2024-01-13 16:22:13 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+
+export const APIurl = "https://smart-brain-recognition-api.onrender.com";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  });
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayBox = (box) => {
+    setBox(box);
+  };
+
+  const onRouteChange = (route) => {
+    if (route === "signout") {
+      setIsSignedIn(false);
+      setUser({});
+      setImageURL("");
+      setRoute("signin");
+    } else if (route === "home") {
+      setIsSignedIn(true);
+    } else {
+      setRoute(route);
+    }
+  };
+
+  const handleButtonSubmit = () => {
+    setImageURL(input);
+    fetch(`${APIurl}/imageurl`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        fetch(`${APIurl}/image`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: user.id,
+          }),
+        })
+          .then((response) => response.json())
+          .then((count) => setUser({ ...user, entries: count }))
+          .catch(console.log);
+        displayBox(calculateFaceLocation(result));
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  const handleLoadUser = (data) => {
+    setUser({
+      id: +data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBg type="cobweb" bg={true} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+
+      {isSignedIn ? (
+        <>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={handleButtonSubmit}
+          />
+          <FaceRecognition box={box} imageURL={imageURL} />
+        </>
+      ) : (
+        <>
+          {route === "signin" && (
+            <Signin onRouteChange={onRouteChange} onLoadUser={handleLoadUser} />
+          )}
+          {route === "register" && (
+            <Register
+              onRouteChange={onRouteChange}
+              onLoadUser={handleLoadUser}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Register/Register.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { APIurl } from "../../App";
+
+const Register = ({ onRouteChange, onLoadUser }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const onSubmitRegister = () => {
+    fetch(`${APIurl}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          onLoadUser(user);
+          onRouteChange("home");
+        }
+      });
+  };
+
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+                onChange={onNameChange}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={onEmailChange}
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={onPasswordChange}
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitRegister}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
+};
+
+export default Register;
